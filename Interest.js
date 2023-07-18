b9786257d289ea7c54c0ab0da94318d105fb4185
Khadim Hussain <118330060+KhadimHussain-99@users.noreply.github.com>
2023-07-18 09:07:41 +0500

    Create Interest.js

---
+++ Interest.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interest(){
+    return(
+        <section>
+            <h2 className="heading">Interest</h2>
+            <p className="paragraph">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </section>
+    )
+}
